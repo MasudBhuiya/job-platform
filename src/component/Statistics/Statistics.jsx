70d6ade75a8648f4fc9a94d7be86b9136f96982e
MasudBhuiya <masudbhuiyan1415@gmail.com>
2023-04-11 13:57:41 +0600

    added randerChart

--- src/component/Statistics/Statistics.jsx
+++ src/component/Statistics/Statistics.jsx
@@ -1,57 +1,95 @@
 import React from 'react';
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, ComposedChart, Area, Line, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, Radar } from 'recharts';
 
 
 const Statistics = () => {
-    const marks = [
+    // const marks = [
+    //     {
+    //         "id": 21,
+    //         "mark": 53,
+    //         "name": 'assignment-one',
+    //         "fullMark":60
+    //     },
+    //     {
+    //         "id":32,
+    //         "mark": 56,
+    //         "name": 'assignment-one',
+    //         "fullMark":60
+    //     },
+    //     {
+    //         "id": 43,
+    //         "mark": 60,
+    //         "name": 'assignment-three',
+    //         "fullMark":60
+    //     },
+    //     {
+    //         "id":54,
+    //         "mark": 60,
+    //         "name": 'assignment-four',
+    //         "fullMark":60
+    //     },
+    //     {
+    //         "id": 45,
+    //         "mark": 54,
+    //         "name": 'assignment-five',
+    //         "fullMark":60
+    //     },
+    //     {
+    //         "id": 36,
+    //         "mark": 58,
+    //         "name": 'assignment-six'
+    //     },
+    //     {
+    //         "id": 27,
+    //         "mark": 60,
+    //         "name": 'assignment-seven',
+    //         "fullMark":60
+    //     },
+    // ]
+
+    const data = [
         {
-            id: '1',
-            mark: '53',
-            name: 'assignment-one'
+          "subject": "assignment-one",
+          "mark": 53,
+          "fullMark": 60
         },
         {
-            id: '2',
-            mark: '56',
-            name: 'assignment-one'
+          "subject": "assignment-two",
+          "mark": 56,
+          "fullMark": 60
         },
         {
-            id: '3',
-            mark: '60',
-            name: 'assignment-three'
+          "subject": "assignment-three",
+          "mark": 60,
+          "fullMark": 60
         },
         {
-            id: '4',
-            mark: '60',
-            name: 'assignment-four'
+          "subject": "assignment-four",
+          "mark": 60,
+          "fullMark": 60
         },
         {
-            id: '5',
-            mark: '54',
-            name: 'assignment-five'
+          "subject": "assignment-five",
+          "mark": 54,
+          "fullMark": 58
         },
         {
-            id: '6',
-            mark: '58',
-            name: 'assignment-six'
-        },
-        {
-            id: '7',
-            mark: '60',
-            name: 'assignment-seven'
-        },
+          "subject": "assignment-six",
+          "mark": 58,
+          "fullMark": 60
+        }
     ]
     return (
         <div className='flex justify-center mt-12 '>
-            <BarChart
-        width={500}
-        height={300}
-        data={marks}
-        >
-            <Bar dataKey="mark" fill='#8884d8'></Bar>
-            <XAxis dataKey="id"></XAxis>
-            <YAxis></YAxis>
-            <Tooltip></Tooltip>
-        </BarChart>
+            <RadarChart outerRadius={90} width={730} height={250} data={data}>
+  <PolarGrid />
+  <PolarAngleAxis dataKey="subject" />
+  <PolarRadiusAxis angle={30} domain={[0, 65]} />
+
+  <Radar name="FullMark" dataKey="fullMark" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+  <Radar name="Mark" dataKey="mark" stroke="#82ca9d" fill="#82ca9d" fillOpacity={0.6} />
+  <Legend />
+</RadarChart>
         </div>
     );
 };
